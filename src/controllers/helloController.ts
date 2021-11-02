e817848fec3ad9cb851714204241efa0b93f03e2
kousuke takahashi <pamarikomi_t@icloud.com>
2021-11-02 23:14:38 +0900

    setting eslint, prettierrc

--- src/controllers/helloController.ts
+++ src/controllers/helloController.ts
@@ -3,8 +3,12 @@ import type { APIGatewayProxyHandler } from 'aws-lambda';
 export const hello: APIGatewayProxyHandler = async (_event, _context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: "helthcheck"
-    }, null, 2),
+    body: JSON.stringify(
+      {
+        message: 'helthcheck'
+      },
+      null,
+      2
+    )
   };
 };
